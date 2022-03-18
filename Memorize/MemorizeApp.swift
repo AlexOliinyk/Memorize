1b2ab1950d89b49d794a32e5e0dce4cc31a3a2f3
AlexOliinyk <alex.oliinyk@gmail.com>
2022-03-18 19:36:02 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Oleksandr Oliinyk on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
